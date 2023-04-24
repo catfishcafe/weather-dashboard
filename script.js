32431a936c9733b5f2ea3015db11ab522d26afb2
catfishcafe <akmhobbs@gmail.com>
2023-04-24 16:21:51 -0400

    can grab city lon and lat

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
+//test
+let lonLatApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=raleigh&limit=1&appid=765cbd98b5ddd0048eff53b809045cdc'
+fetch(lonLatApiUrl)
+.then(response => response.json())
+.then(searchedCity => 
+    console.log('lon: ' + searchedCity[0].lon + 'lat: ' + searchedCity[0].lat)
+    )
+.catch(err => { throw err });
+
+let weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=35.7803977&lon=-78.6390989&appid=765cbd98b5ddd0048eff53b809045cdc';
+fetch(weatherUrl)
+.then(response => response.json())
+.then(city => 
+    console.log(city)
+    )
+.catch(err => { throw err });
+//test
+
 const srcForm = document.getElementById('srcFormEl');
 const srcBar = document.getElementById('srcBarEl');
 
-//capture the search once the button is clicked
-//captured search will go to: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
 let srcSubmitHandler = function (event) {
     event.preventDefault();
-    let city = srcBar.value.trim();
-    // if (city) {
-    //     getWeather(cityName);
-    //⭐️⭐️Pass city into lon lat converter
-
-    // }
-    console.log(city);
+    let searchedCity = srcBar.value.trim();
+    if (searchedCity){
+        getCityLonLat(searchedCity);
+        
+        srcBar.value = '';
+        //I need to clear the search results too but not sure how :/
+    } else {
+        alert('Please enter the name of a city.')
+    }
 };
 
-// ⭐️ below: example
-// var formSubmitHandler = function (event) {
-//     event.preventDefault();
-  
-//     var username = nameInputEl.value.trim();
-  
-//     if (username) {
-//       getUserRepos(username);
-  
-//       repoContainerEl.textContent = '';
-//       nameInputEl.value = '';
-//     } else {
-//       alert('Please enter a GitHub username');
-//     }
-//   };
-// end example ⭐️
-
-
-//⭐️lat and lon are placeholders
-let getWeather = function(city){
-    let weatherURl = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' 
-    + lon + '&appid=765cbd98b5ddd0048eff53b809045cdc';
-    fetch 
+let getCityLonLat = function(city){
+    let lonLatApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=765cbd98b5ddd0048eff53b809045cdc'
+    fetch(lonLatApiUrl)
+    .then(response => response.json())
+    .then(city => function(){
+        let cityLon = city[0].lon;
+        let cityLat =  city[0].lat;
+        getWeather(cityLon, cityLat);
+        console.log(getWeather);
+    })
+    .catch(function (error) {
+  alert('Unable to connect to OpenWeather');
+    });    
 }
 
-// var getUserRepos = function (user) {
-//     var apiUrl = 'https://api.github.com/users/' + user + '/repos';
-  
-//     fetch(apiUrl)
-//       .then(function (response) {
-//         if (response.ok) {
-//           response.json().then(function (data) {
-//             displayRepos(data, user);
-//           });
-//         } else {
-//           alert('Error: ' + response.statusText);
-//         }
-//       })
-//       .catch(function (error) {
-//         alert('Unable to connect to GitHub');
-//       });
-//   };
+let getWeather = function(cityLon, cityLat){
+    let weatherUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' 
+    + cityLon + '&appid=765cbd98b5ddd0048eff53b809045cdc';
+    fetch(weatherUrl)
+    .then(response => response.json())
+    .then(city => function(){
+        console.log(wait)
+    })
+}
   
 srcForm.addEventListener('submit', srcSubmitHandler);
